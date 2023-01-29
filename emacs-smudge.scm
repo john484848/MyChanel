fca9ab0a908bd184b2910244c22413648de8937f
Jonathan Kloepfer <kloepj@rpi.edu>
2023-01-28 20:30:24 -0500

    H

---
+++ emacs-smudge.scm
@@ -0,0 +1,28 @@
+(define-module (emacs-smudge)
+  #:use-module (guix)
+  #:use-module (guix licenses)
+  ;;#:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix build-system emacs))
+(define-public emacs-smudge
+  (package
+   (name "emacs-smudge")
+   (version "1.0")
+   (source (origin
+	    (method git-fetch)
+	    (uri (git-reference
+		  (url "https://github.com/danielfm/smudge.git")
+		  (commit "9e3488f485b7d7f3c97ebaad34ed552bb0cc228a")))
+	    (sha256
+	     (base32
+	      "17sgnl5avnxfw7hdl7k3ssqcj2ygrhfwg2rv0krn7axmjlmr2gjp"))
+	    (file-name (git-file-name name version))))
+	    (build-system emacs-build-system)
+	    (home-page "https://github.com/danielfm/smudge")
+	    (synopsis
+	     "An emacs package to control spotify")
+	    (description
+	     "An emacs package to control spotify")
+            (license license:gpl3)
+   ))
